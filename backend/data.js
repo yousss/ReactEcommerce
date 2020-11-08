38d5bd27b0a51d5dec0824c677a4b512e98a2af6
Yousss <yous.yoeunfrence@gmail.com>
2020-11-08 19:54:15 +0700

    add new init commit

---
+++ backend/data.js
@@ -0,0 +1,65 @@
+
+const products = [
+    {
+        _id:1,
+        name:'testone',
+        category: 'One',
+        image: '/images/p1.jpg',
+        price: 120,
+        branch: 'Nice',
+        rating: 4.5,
+        numReviews: 10,
+        countInstock: 10,
+        description: 'high quality product'
+    },
+    {
+        _id: 2,
+        name: 'Nice taste',
+        category: 'two',
+        image: '/images/p2.jpg',
+        price: 150,
+        branch: 'Nice',
+        rating: 4.5,
+        numReviews: 10,
+        countInstock: 200,
+        description: 'high quality product'
+    },
+    {
+        _id: 3,
+        name: 'double taste',
+        category: 'One',
+        image: '/images/p3.jpg',
+        price: 160,
+        branch: 'Nice',
+        rating: 4.0,
+        numReviews: 10,
+        countInstock: 0,
+        description: 'high quality product'
+    },
+    {
+        _id: 4,
+        name: 'Third Tea',
+        category: 'One',
+        image: '/images/p4.jpg',
+        price: 170,
+        branch: 'Nice',
+        rating: 3.5,
+        numReviews: 10,
+        countInstock: 600,
+        description: 'high quality product'
+    },
+    {
+        _id: 5,
+        name: 'nice hong kong taste',
+        category: 'One',
+        image: '/images/p5.jpg',
+        price: 180,
+        branch: 'Liga',
+        rating: 4.5,
+        numReviews: 10,
+        countInstock: 25,
+        description: 'high quality product'
+    }
+];
+
+module.exports = products
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const products = require("./data");
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(x => x._id === parseInt(req.params.id))
+    if(product) {
+        res.send(product)
+    } else {
+        res.status(404).send({ message: 'Product not found' })
+    }
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Server is ready')
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log('Server is at http://localhost' + port)
+})
